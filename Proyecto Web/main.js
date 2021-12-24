5d49cddd60db7e20cd029fd787a3f4a25546247c
MisaelMota <87102180+MisaelMota@users.noreply.github.com>
2021-12-24 09:39:56 -0400

    Add files via upload

---
+++ Proyecto Web/main.js
@@ -0,0 +1,13 @@
+document.querySelector('.menu-btn').addEventListener('click',() => {
+    document.querySelector('.Nav-menu').classList.toggle('show'); 
+
+
+})
+
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-cards',{delay:500});
+ScrollReveal().reveal('.cards-banner-one',{delay:500});
+ScrollReveal().reveal('.cards-banner-two',{delay:500});
+ScrollReveal().reveal('.Social',{delay:500});
+ScrollReveal().reveal('.footer-container',{delay:500});
